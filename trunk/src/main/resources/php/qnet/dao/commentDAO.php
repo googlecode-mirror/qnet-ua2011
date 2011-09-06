e725b96efe90a01a0e293f1977d90402d2bc189f
martinlsanchez@gmail.com <martinlsanchez@gmail.com@9881820e-2cd8-1ace-f727-7bfbafeba39c>
2011-09-06 00:33:11 +0000

    added commentsTest.php - MS

--- trunk/src/main/resources/php/qnet/dao/commentDAO.php
+++ trunk/src/main/resources/php/qnet/dao/commentDAO.php
@@ -1,37 +1,42 @@
 <?php
 namespace Qnet\Dao;
 
-    require_once dirname(__FILE__) . '\..\util.php';
-    require_db();
+require_once dirname(__FILE__) . '\..\util.php';
+require_db();
 
-    class CommentDAO {
+class CommentDAO
+{
 
-        public function saveComment($qId, $uId, $text) {
-            $connector = new DBConnector();
-            $connection = $connector->createConnection();
-            $date = date('Y-m-d H:i:s');
-            $query = "INSERT INTO comment (text,FK_userId,FK_queryId, date) VALUES ('$text','$uId','$qId', '$date')";
-            mysql_query($query) or die("Error in query: $query. " . mysql_error());
-            mysql_close($connection);
+    public function saveComment($qId, $uId, $text)
+    {
+        $connector = new DBConnector();
+        $connection = $connector->createConnection();
+        $date = date('Y-m-d H:i:s');
+        $query = "INSERT INTO comment (text,FK_userId,FK_queryId, date) VALUES ('$text','$uId','$qId', '$date')";
+        mysql_query($query) or die("Error in query: $query. " . mysql_error());
+        $lastId = mysql_insert_id();
+        mysql_close($connection);
+        return $lastId;
+    }
+
+
+    public function loadAllComments($qId)
+    {
+        $connector = new DBConnector();
+        $connection = $connector->createConnection();
+        $result = mysql_query("SELECT * FROM comment WHERE FK_queryId='$qId'") or die ("Error"); //TODO DATE
+        $ans = array();
+        while ($row = mysql_fetch_assoc($result)) {
+            $ans[$row['id']] = $row['text'];
         }
+        mysql_free_result($result);
+        mysql_close($connection);
 
+        return $ans;
 
-        public function loadAllComments($qId) {
-            $connector = new DBConnector();
-            $connection = $connector->createConnection();
-            $result = mysql_query("SELECT * FROM comment WHERE FK_queryId='$qId'") or die ("Error"); //TODO DATE
-            $ans = array();
-            while($row = mysql_fetch_assoc($result)) {
-                $ans[$row['id']] = $row['text'];
-            }
-            mysql_free_result($result);
-            mysql_close($connection);
-
-            return $ans;
-
-
-        }
 
     }
 
-    ?>
+}
+
+?>
---
+++ trunk/src/test/resources/php/qnet/dao/commentsTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Mart0
+ * Date: 9/5/11
+ */
+require_once dirname(__FILE__) . '\..\util.php';
+require_dao('commentDAO');
+use Qnet\Dao\CommentDAO;
+$commentDao = new CommentDAO();
+$newCommentId = $commentDao->saveComment(1, 1, "test");
+$commentMap = $commentDao->loadAllComments(1);
+if ($commentMap[$newCommentId] == "test") {
+    // ALL GOOD
+} else {
+    echo "Comment Test Fail";
+}
+
+?>
