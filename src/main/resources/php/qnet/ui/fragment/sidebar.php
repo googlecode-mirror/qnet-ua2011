9f8e2a10a1e9d1cf137adfa523fcad5fe83275f3
martinlsanchez <martinlsanchez@gmail.com>
2011-11-07 22:59:17 +0000

    Bug 33 Fixed

--- src/main/resources/php/qnet/ui/fragment/sidebar.php
+++ src/main/resources/php/qnet/ui/fragment/sidebar.php
@@ -6,7 +6,7 @@ echo "<h2>" . $header->getUserName();
 if ($header->canFollow()) {
     echo '<input id="followBtt" type="button"  class="button2" onClick="follow(' . $header->getUserID() . ')" style="font-size: 0.7em;" value="Follow!" /></h2>';
 }
-echo "</h2><ul><li><img width=\"120\" height=\"150\" \"src='.." . $photoPath . "' alt='photo'></li></ul>";
+echo "</h2><ul><li><img width=\"120\" height=\"150\" src='.." . $photoPath .  "' alt='photo'></li></ul>";
 echo '
 
                 </li>';
@@ -23,10 +23,13 @@ if ($header->isOwnProfile()) {
                     <h2>Account Options</h2>
                     <ul>';
     echo '<li><a href="modifyUser.php">Edit profile</a></li>';
-    echo '<li><form enctype="multipart/form-data" action="../controller/photoController.php?actionId=1" method="POST">
+    echo '<li><a id="photo"/><form enctype="multipart/form-data" action="../controller/photoController.php?actionId=1" method="POST">
 <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
-Choose a file to upload: <input name="uploadedfile" type="file" /><br />
-<input type="submit" value="Upload File" />
+Choose a file to upload: <input name="uploadedfile" type="file" /><br />';
+    if ($_GET[photoUpload] == "wrong") {
+        echo "<div style='color:red'>Error Upload Photo (invalid Ext or the file is too big) </div>";
+    }
+    echo '<input type="submit" value="Upload File" />
 </form>
 </li>';
     echo "<li><a href='bridge.php?target=deleteAccount'>Delete account</a>";
